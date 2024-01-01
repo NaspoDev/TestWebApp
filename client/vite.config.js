dd52f3614fc45f4ce6fb648ae2a1111d335befff
Naspo <a.topaltsis@gmail.com>
2024-01-01 18:40:40 -0500

    Updated comments for proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       // Set this to the IP of your backend.
+      // In this example, my app is running on testwebapp.naspoapps.com, and the
+      // backend will be running on testwebapp.naspoapps.com/api.
       // During development you would set this to http://localhost:5000 for example.
       "/api": "https://testwebapp.naspoapps.com", // aws vps running our node.js app.
     },
